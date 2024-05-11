3ce584c5d07c65ced260c614bd6f01997bfd32db
SHIRISH <shirish.shrestha07@gmail.com>
2024-05-11 12:36:14 +0545

    changes

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
--- frontend/product_ordering/src/utils/Instance.js
+++ frontend/product_ordering/src/utils/Instance.js
@@ -2,6 +2,5 @@ import axios from "axios";
 const ApiUrl = import.meta.env.VITE_APP_API;
 const instance = axios.create({
   baseURL: ApiUrl,
-  withCredentials: true,
 });
 export default instance;
