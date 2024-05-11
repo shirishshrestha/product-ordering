8ff987d0644a2ce5df0cab08ea30c4b4a9b78ac4
SHIRISH <shirish.shrestha07@gmail.com>
2024-05-11 11:30:29 +0545

    complete: ui of product ordering

--- frontend/product_ordering/src/pages/Products/Products.jsx
+++ frontend/product_ordering/src/pages/Products/Products.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { getProducts } from "../../api/ProductApiSlice";
+import "./Products.css";
 
 import { placeOrderReq } from "../../api/PlaceOrderApiSlice";
 
@@ -41,37 +42,68 @@ function Products() {
 
   return (
     <div className="App">
-      <h1>Product Ordering System</h1>
-      <h2>Select Products</h2>
-      <ul>
+      <div className="heading">
+        <h1 className="product__heading">Product Ordering System</h1>
+        <h2>Select Products</h2>
+      </div>
+      <div className="product__list">
         {isPending ? (
-          <p>Loading...</p>
+          <div className="loading">
+            <p className="loading__para">Loading...</p>
+          </div>
         ) : (
           products?.map((product) => (
-            <li key={product.id}>
-              <input
-                type="checkbox"
-                checked={selectedProducts.some((p) => p.id === product.id)}
-                onChange={() => handleCheckboxChange(product)}
-              />
-              {product.name} - ${product.price} - {product.weight}g
-            </li>
+            <div className="product__content" key={product.id}>
+              <div className="product__inputs">
+                <input
+                  type="checkbox"
+                  checked={selectedProducts.some((p) => p.id === product.id)}
+                  onChange={() => handleCheckboxChange(product)}
+                />
+                <p> {product.name}</p>
+              </div>
+              <p className="product__details">
+                <span>Price:</span> ${product.price}
+              </p>
+              <p className="product__details">
+                <span>Weight:</span> {product.weight}g
+              </p>
+            </div>
           ))
         )}
-      </ul>
-      <button onClick={placeOrder}>Place Order</button>
-      <h2>Packages</h2>
-      <ul>
-        {packages?.map((pkg, index) => (
-          <li key={index}>
-            <h3>Package {index + 1}</h3>
-            <p>Items: {pkg.items.map((item) => item.name).join(", ")}</p>
-            <p>Total weight: {pkg.totalWeight}g</p>
-            <p>Total price: ${pkg.totalPrice}</p>
-            <p>Courier price: ${pkg.courierPrice}</p>
-          </li>
-        ))}
-      </ul>
+      </div>
+      <div className="button__div">
+        <button
+          disabled={selectedProducts.length < 1}
+          className={selectedProducts.length < 1 ? "disabled button" : "button"}
+          onClick={placeOrder}
+        >
+          Place Order
+        </button>
+      </div>
+      <div className="package">
+        <h2 className="package__heading">Packages</h2>
+        <div className="package__details">
+          {packages?.map((pkg, index) => (
+            <div className="package__content" key={index}>
+              <h3 className="package__content--heading">Package {index + 1}</h3>
+              <p className="package__para">
+                <span>Items: </span>
+                {pkg.items.map((item) => item.name).join(", ")}
+              </p>
+              <p className="package__para">
+                <span>Total weight: </span> {pkg.totalWeight}g
+              </p>
+              <p className="package__para">
+                <span>Total price: </span> ${pkg.totalPrice}
+              </p>
+              <p className="package__para">
+                <span>Courier price:</span> ${pkg.courierPrice}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
